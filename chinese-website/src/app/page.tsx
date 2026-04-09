7fef33d529fbc1da90e1aac8c0c5a6a80f750767
RowanMadeeh <102481584+RowanMadeeh@users.noreply.github.com>
2026-04-09 10:50:22 +0200

    removed footer

--- chinese-website/src/app/page.tsx
+++ chinese-website/src/app/page.tsx
@@ -28,7 +28,6 @@ const App = () => (
         <EntertainmentSection />
         <FoodSection />
         <FestivalsSection />
-        <Footer />
       </div>
     </TooltipProvider>
   </QueryClientProvider>
