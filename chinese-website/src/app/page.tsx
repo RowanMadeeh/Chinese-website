04a8ddb43e76988f6333037ff93ad3d107b0b7a7
RowanMadeeh <102481584+RowanMadeeh@users.noreply.github.com>
2026-04-09 10:16:22 +0200

    fix plcaement for deployment

--- chinese-website/src/app/page.tsx
+++ chinese-website/src/app/page.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster as Sonner } from "@/app/components/ui/sonner";
 import { Toaster } from "@/app/components/ui/toaster";
 import { TooltipProvider } from "@/app/components/ui/tooltip";
-import Index from "@/app/pages/index";
+import Index from "@/pages/index";
 
 const queryClient = new QueryClient();
 
